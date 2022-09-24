755f33b75a1db343596698c4b3370ec74ca8ce14
MurtazaNeher <murtazaradio@gmail.com>
2022-09-24 23:37:11 +0530

    header done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,23 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".main-nav .nav-wrapper .nav-list li a");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((a) => {
+    a.classList.remove("active");
+    if (a.classList.contains(current)) {
+      a.classList.add("active");
+    }
+
+    console.log(current)
+  });
+};
 var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
